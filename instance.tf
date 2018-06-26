6b1905e1b3036ece3c79df00a172451b2561c273
nicorouanet <nicolas1.rouanet@gmail.com>
2018-06-26 12:58:26 +0200

    ajout sub privée et public (activation ip public true et false)

--- instance.tf
+++ instance.tf
@@ -1,4 +1,97 @@
-resource "aws_instance" "exemple" {	
-	ami		= "ami-14c5486b"
-	instance_type = "t2.micro"
+#VPC
+resource "aws_vpc" "main" {	
+	enable_dns_support = "true"
+	enable_dns_hostnames = "true"
+	cidr_block = "${var.AWS_CIDR}"
+	instance_tenancy = "default"
 }
+
+#gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+#route table
+resource "aws_route_table" "route-t" {
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+  }
+  
+ #Subnet public
+  resource "aws_subnet" "subpublic_1" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${var.AWS_CIDR_2}"
+  map_public_ip_on_launch = "true"
+   tags {
+    Name = "Public Subnet"
+	}
+    }
+	
+#Subnet private
+  resource "aws_subnet" "subprivee_1" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${var.AWS_CIDR_3}"
+  map_public_ip_on_launch = "false"
+   tags {
+    Name = "Privée Subnet"
+	}
+    }
+	
+  
+  #elastic IP
+  resource "aws_eip" "EIP" {
+  vpc      = true
+}
+
+#NAT
+	resource "aws_nat_gateway" "nat" {
+	allocation_id = "${aws_eip.EIP.id}"
+	subnet_id     = "${aws_subnet.subpublic_1.id}"
+	depends_on = ["aws_internet_gateway.gw"]
+	
+}
+	
+#route table NAT
+resource "aws_route_table" "nat_aws" {
+    vpc_id = "${aws_vpc.main.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = "${aws_nat_gateway.nat.id}"
+    }
+
+		#tags {
+			#Name = "RT NAT"
+    #}
+}	
+	
+	
+	
+	
+# Route associations private
+resource "aws_route_table_association" "pri_associate1" {
+    subnet_id = "${aws_subnet.subprivee_1.id}"
+    route_table_id = "${aws_route_table.route-t.id}"
+}
+	
+	
+	
+# Route associations public
+resource "aws_route_table_association" "pub_associate1" {
+    subnet_id = "${aws_subnet.subpublic_1.id}"
+    route_table_id = "${aws_route_table.route-t.id}"
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- vars.tf
+++ vars.tf
@@ -1,2 +1,5 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
+variable "AWS_CIDR" {}
+variable "AWS_CIDR_2" {}
+variable "AWS_CIDR_3" {}
